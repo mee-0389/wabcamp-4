79e5f5e6c0129e9ce3cb26e59515ecc34bcd79aa
mee-0389 <139702797+mee-0389@users.noreply.github.com>
2023-08-17 05:28:28 +0000

    [Update] 14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   #devise利用の機能が使われる前にconfigue...メソッドが実行される。
 
+  def after_sign_in_path_for(resource)
+    post_images_path
+  end
+
+  def after_sign_out_path_for(resource)#サインアウト後どこに遷移するかを設定
+    about_path
+  end
+
   protected
 
   def configure_permitted_parameters
